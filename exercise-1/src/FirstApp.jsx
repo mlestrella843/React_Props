264640e8f21199b9c2dfa4f0c68f65a1ceb94104
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-07-19 14:07:20 -0600

    Using variables

--- exercise-1/src/FirstApp.jsx
+++ exercise-1/src/FirstApp.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
+//Variable con solo un mensaje
 const newMesage = "Hola Maria!!!!"
 
+//Variable tipo ibjeto
 const newMessage2 = {
     message: "Estamos aprendiendo",
     title: "Aprendiendo"
+}
+
+//Funcion que pide dos numeros y se ejecuta en el cuerpo del componente
+const getResult = (a,b) => {
+    return a + b;
 }
 
 export const FirstApp = () => {
@@ -14,6 +21,7 @@ export const FirstApp = () => {
     <> 
         <h1>First App!!!</h1>
         <h2>Operacion: {newMessage2.title}</h2>
+        <h2>Operacion: { getResult(1,23) }</h2>
         <h3>Mi nombre es Maria</h3>
         <p> Este es un subtitulo</p>
    </>
