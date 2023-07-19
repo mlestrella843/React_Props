c5cff3debcf06503c33745ddfd4800acc0621006
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-07-19 13:59:12 -0600

    Objects

--- exercise-1/src/FirstApp.jsx
+++ exercise-1/src/FirstApp.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
+const newMesage = "Hola Maria!!!!"
+
+const newMessage2 = {
+    message: "Estamos aprendiendo",
+    title: "Aprendiendo"
+}
+
 export const FirstApp = () => {
   return (
-   <h1>First App!!!</h1>
+    // Este simbolo <> hace la funcion de un Fragment, que evita tener que usar un div de mas.
+    // Funciona como un agrupador Padre de los demas elementos HTML
+    <> 
+        <h1>First App!!!</h1>
+        <h2>Operacion: {newMessage2.title}</h2>
+        <h3>Mi nombre es Maria</h3>
+        <p> Este es un subtitulo</p>
+   </>
+    // Aqui se cierra el Fragment
   )
+
 }
