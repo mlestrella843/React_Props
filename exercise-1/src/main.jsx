04d4e0ccbc5fa3f3ebe449b1719f33d2fe83591a
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-07-30 01:18:09 -0600

    Adding Test React Library installation

--- exercise-1/src/main.jsx
+++ exercise-1/src/main.jsx
@@ -6,11 +6,11 @@ import ReactDOM from 'react-dom/client'; //Herramienta para renderizar
 //import { App } from './helloWorldApp'//Esta importada de manera independiente, por eso las llaves.
 //import App from './helloWorldApp';
 //import { HelloWorldApp } from './helloWorldApp'
-//import { FirstApp } from './FirstApp';
+import { FirstApp } from './FirstApp';
 import './style.css';
 //import { CounterApp } from './CounterApp';
 //import { FirstHook } from './firstHook';
-import { KeepHooking } from './KeepHooking';
+// import { KeepHooking } from './KeepHooking';
 
 //Componente que regularmente nunca esta dentro de este archhivo
 
@@ -25,11 +25,11 @@ ReactDOM.createRoot( document.getElementById('root') ).render(
 
         {/* Desde aqui estamos mandando informacion desde el componente padre hacia el hijo
         llamado FirstApp */}
-        {/* <FirstApp title="Maria" subtitle="Estoy probando"/> */}
+        <FirstApp title="Maria" subtitle="Estoy probando"/>
         {/* <CounterApp  value={ 234 }/> */}
         {/* <FirstHook value={ 20 }/> */}
         
-        <KeepHooking value={ 20 }/>
+        {/* <KeepHooking value={ 20 }/> */}
 
 
     </React.StrictMode>
